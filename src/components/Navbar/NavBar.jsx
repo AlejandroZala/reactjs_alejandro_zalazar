30c8bc17898ae7f1a61a9ee0d89dce436cc05a6d
Alejandro Zalazar <alejandrodzalazar@gmail.con>
2022-09-08 21:47:06 -0300

    armado archivo NavBar.jsx y App.js con el import del NavBar

---
+++ src/components/Navbar/NavBar.jsx
@@ -0,0 +1,18 @@
+function NavBar() {
+    return (
+        <nav className="navBar">
+            <h1>
+                <strong>Negocios Inmobiliarios</strong>
+            </h1>
+            <ul>
+                <li>Inicio</li>
+                <li>Nosotros</li>
+                <li>Ventas</li>
+                <li>Alquileres</li>
+                <li>Contacto</li>
+                <li>Ubicación</li>
+            </ul>
+        </nav>
+    )
+}
+export default NavBar;
